3bd538cb9b77f947d27fa03c7c07053f3a3a684c
佚名 <2251532000@qq.com>
2024-09-29 14:28:41 +0800

    大型聚爆应力厂

---
+++ kubejs/server_scripts/src/boom.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of('enderio:basic_energy_conduit', 2), [
+        "DED",
+        "ACA",
+        "BAB"
+      ], {
+        A: 'gtceu:iv_conveyor_module',
+        B: 'gtceu:crystal_processor',
+        C: 'gtceu:iv_machine_hull',
+        D: 'gtceu:iv_electric_pump',
+        E: 'gtceu:big_dam'
+      })
+})
