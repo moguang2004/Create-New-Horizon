a8f93927e7c73092e6467c9f0fc8e359a10d53bd
moguang <184676648@qq.com>
2024-10-17 11:02:56 +0800

    update CTNHcore

--- kubejs/server_scripts/src/event.js
+++ kubejs/server_scripts/src/event.js
@@ -1,6 +1,3 @@
 ItemEvents.foodEaten('farmersdelight:rice', event =>{
     event.player.give('kubejs:crashed_rice')
 })
-// ItemEvents.rightClicked('mythicbotany:asgard_rune', event=>{
-//     console.info(event.getItem().toString())
-// })
