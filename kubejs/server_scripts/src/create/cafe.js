3d8c856632dc96e51fd37ded609c866fd57257ea
又吓雪 <3661445846@qq.com>
2025-01-16 00:44:26 +0800

    Add files via upload

---
+++ kubejs/server_scripts/src/create/cafe.js
@@ -0,0 +1,36 @@
+ServerEvents.recipes(e => {
+    e.recipes.create.mixing(Fluid.of('createcafe:strawberry_tea', 500),
+                            ['1x farm_and_charm:strawberry',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 250)]).heated()
+
+    e.recipes.create.mixing(Fluid.of('createcafe:strawberry_syrup', 1000),
+                            ['1x farm_and_charm:strawberry',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 750)]).heated()
+
+    e.recipes.create.mixing(Fluid.of('createcafe:blueberry_tea', 500),
+                            ['1x aether:blue_berry',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 250)]).heated()
+
+    e.recipes.create.mixing(Fluid.of('createcafe:coconut_tea', 500),
+                            ['1x ecologics:coconut_slice',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 250)]).heated()
+
+    e.recipes.create.mixing(Fluid.of('createcafe:pomegranate_tea', 500),
+                            ['1x ars_nouveau:bombegranate_pod',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 250)]).heated()
+
+    e.recipes.create.mixing(Fluid.of('createcafe:blood_tea', 500),
+                            ['1x gtceu:snow_steel_ingot',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 250)]).heated()
+
+    e.recipes.create.mixing(Fluid.of('createcafe:cherry_tea', 500),
+                            ['1x seeddelight:cherry',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 250)]).heated()
+})
