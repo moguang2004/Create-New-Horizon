4a62750d715e7930abf5d16f862c8436e9b94e85
又吓雪 <3661445846@qq.com>
2025-01-16 10:20:25 +0800

    Add files via upload

--- kubejs/server_scripts/src/create/cafe.js
+++ kubejs/server_scripts/src/create/cafe.js
@@ -19,6 +19,11 @@ ServerEvents.recipes(e => {
                             Fluid.of('minecraft:milk', 250), 
                             Fluid.of('createcafe:melted_sugar', 250)]).heated()
 
+    e.recipes.create.mixing(Fluid.of('createcafe:coconut_syrup', 1000),
+                            ['1x farm_and_charm:strawberry',
+                            Fluid.of('minecraft:milk', 250), 
+                            Fluid.of('createcafe:melted_sugar', 750)]).heated()
+                            
     e.recipes.create.mixing(Fluid.of('createcafe:pomegranate_tea', 500),
                             ['1x ars_nouveau:bombegranate_pod',
                             Fluid.of('minecraft:milk', 250), 
