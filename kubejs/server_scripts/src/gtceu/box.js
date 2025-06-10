549c34bf48fa8d4e11809a7798d6a1fe4c02993f
moguang <184676648@qq.com>
2025-06-11 00:19:53 +0800

    该死的expanded storage

--- kubejs/server_scripts/src/gtceu/box.js
+++ kubejs/server_scripts/src/gtceu/box.js
@@ -1,15 +1,15 @@
-ServerEvents.recipes(event => {
-    let ctnh = event.recipes.gtceu
-    let material = ['iron', 'gold', 'diamond']
-    material.forEach(m => {
-        ctnh.assembler(m + '_chest')
-            .itemInputs(['4x gtceu:' + m + '_plate', "1x minecraft:chest"])
-            .circuit(4)
-            .itemOutputs('expandedstorage:' + m + '_chest')
-            .EUt(120)
-            .duration(50)
-    })
-})
+// ServerEvents.recipes(event => {
+//     let ctnh = event.recipes.gtceu
+//     let material = ['iron', 'gold', 'diamond']
+//     material.forEach(m => {
+//         ctnh.assembler(m + '_chest')
+//             .itemInputs(['4x gtceu:' + m + '_plate', "1x minecraft:chest"])
+//             .circuit(4)
+//             .itemOutputs('expandedstorage:' + m + '_chest')
+//             .EUt(120)
+//             .duration(50)
+//     })
+// })
 ServerEvents.recipes(event => {
     let ctnh = event.recipes.gtceu
     let voltage = ['lv', 'mv', 'hv', 'ev', 'iv', 'luv', 'zpm', 'uv']
