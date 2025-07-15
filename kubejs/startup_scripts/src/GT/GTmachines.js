29480a039643194fced720a8ab423b328559b9e7
Li Silin <2667023708@qq.com>
2025-07-15 12:00:57 +0800

    修改

--- kubejs/startup_scripts/src/GT/GTmachines.js
+++
@@ -1,9 +0,0 @@
-GTCEuStartupEvents.registry('gtceu:machine', event => {
-    event.create("inscriber", "simple")
-        .tiers(GTValues.MV, GTValues.HV, GTValues.EV, GTValues.IV, GTValues.LuV, GTValues.ZPM, GTValues.UV)
-        .definition((tier,builder) =>{
-            builder.recipeType("inscriber")
-            .workableTieredHullRenderer("gtceu:block/machines/inscriber")
-        })
-        .tankScalingFunction(tier => tier * 4800)
-})
