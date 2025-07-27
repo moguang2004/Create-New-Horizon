71429072c8ec6051a752ca9c2da2e1e6f53d0d7f
mo_guang <112535569+moguang2004@users.noreply.github.com>
2025-07-27 18:56:36 +0800

    Revert "修改II (#160)" (#167)
    This reverts commit f4048302a6fae157824aea4fd3392bac423bb869.

--- kubejs/server_scripts/src/blood magic/kubejs/server_scripts/src/event.js
+++
@@ -1,7 +0,0 @@
-ItemEvents.foodEaten('farmersdelight:rice', event =>{
-    event.player.give('kubejs:crashed_rice')
-})
-
-ItemEvents.foodEaten('kubejs:double_blaze_cake', event =>{
-    event.player.give('create:blaze_cake_base')
-})
