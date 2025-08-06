2146bde0ec18a88167c85a6a740a1efeb4b0dec1
mmyddd <98249707+mmyddd@users.noreply.github.com>
2025-08-06 11:03:48 +0800

    移除匠魂铁矿熔融(副产钢)配方 (#183)
    * 移除匠魂铁矿熔融(副产钢)配方

--- kubejs/server_scripts/src/tconstruct/remove_tinkers_repices.js
+++ kubejs/server_scripts/src/tconstruct/remove_tinkers_repices.js
@@ -17,6 +17,15 @@ ServerEvents.recipes(event => {
         //删除TiC熔融粘土配方
         "tconstruct:smeltery/casting/clay/block",
         /tconstruct:smeltery\/melting\/clay\/(.*)/,
-        /tconstruct:smeltery\/casting\/clay\/brick_(.*)_cast/
+        /tconstruct:smeltery\/casting\/clay\/brick_(.*)_cast/,
+        //移除熔铸炉钢配方
+        "tconstruct:smeltery/melting/metal/iron/chain_boots",
+        "tconstruct:smeltery/melting/metal/iron/chain_chestplate",
+        "tconstruct:smeltery/melting/metal/iron/chain_helmet",
+        "tconstruct:smeltery/melting/metal/iron/chain_leggings",
+        "tconstruct:smeltery/melting/metal/iron/ore_singular",
+        "tconstruct:smeltery/melting/metal/iron/raw",
+        "tconstruct:smeltery/melting/metal/iron/raw_block"
+
     ])
 })
