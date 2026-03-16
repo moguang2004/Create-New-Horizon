c3a9d176f71a74567b1ce48c133de12eac29d625
lucky_block <1774432258@qq.com>
2026-03-17 01:27:00 +0800

    fix: 去除过时的tooltip, 隐藏经典改进的硫酸, 去moguang化

--- kubejs/client_scripts/src/tooltip.js
+++ kubejs/client_scripts/src/tooltip.js
@@ -60,10 +60,8 @@ ItemEvents.tooltip(event => {
 
     event.add('hostilenetworks:deep_learner',Text.translate('ctnh.hostilenetworks.deep_learner').yellow())
     event.add('hostilenetworks:sim_chamber',Text.translate('ctnh.hostilenetworks.sim_chamber').yellow())
-    
-    //Snow_city's cautions:
-    //event.add('gtceu:ender_fluid_link_cover', '§4暂未实装')
-    event.add(['gtceu:item_tag_filter', 'gtceu:fluid_tag_filter'], Text.red("非语句 [!] 不可用"))
+
+
     event.add('gtceu:nightvision_goggles', '§7用Gregtech的 [Armor Mode Switch] 键开启.')
     event.add(['gtceu:lp_steam_macerator', 'gtceu:hp_steam_macerator', 'gtceu:steam_grinder', 'gtceu:lv_macerator', 'gtceu:mv_macerator', 'ctpp:smashing_factory'], '§4研磨副产物只能通过HV及以上的电压获得！')
     event.addAdvanced('tconstruct:crafting_station', (item, advanced,text) => {
