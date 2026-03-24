910fa7771d20f69f79ce7e8a942c8d255bc3bdf2
TonyCrane <me@tonycrane.cc>
2026-03-24 22:44:21 +0800

    fix: 移除缺失输入的配方防止无用报错

--- kubejs/server_scripts/src/create/cafe.js
+++ kubejs/server_scripts/src/create/cafe.js
@@ -1,9 +1,9 @@
 ServerEvents.recipes(e => {
-    e.recipes.create.mixing(Fluid.of('createcafe:strawberry_tea', 500),
-                            ['1x farm_and_charm:strawberry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 250)]).heated()
+    // e.recipes.create.mixing(Fluid.of('createcafe:strawberry_tea', 500),
+    //                         ['1x farm_and_charm:strawberry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 250)]).heated()
 
-    e.recipes.create.mixing(Fluid.of('createcafe:strawberry_syrup', 1000),
-                            ['1x farm_and_charm:strawberry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 750)]).heated()
+    // e.recipes.create.mixing(Fluid.of('createcafe:strawberry_syrup', 1000),
+    //                         ['1x farm_and_charm:strawberry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 750)]).heated()
 
     e.recipes.create.mixing(Fluid.of('createcafe:blueberry_tea', 500),
                             ['1x aether:blue_berry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 250)]).heated()
@@ -20,8 +20,8 @@ ServerEvents.recipes(e => {
     e.recipes.create.mixing(Fluid.of('createcafe:blood_tea', 500),
                             ['1x gtceu:snow_steel_ingot',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 250)]).heated()
 
-    e.recipes.create.mixing(Fluid.of('createcafe:cherry_tea', 500),
-                            ['1x seeddelight:cherry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 250)]).heated()
+    // e.recipes.create.mixing(Fluid.of('createcafe:cherry_tea', 500),
+    //                         ['1x seeddelight:cherry',Fluid.of('minecraft:milk', 250), Fluid.of('createcafe:melted_sugar', 250)]).heated()
 
     // let Ingredients = ['mango','lime','strawberry','peach','plum','jackfruit','apricot','durian','fig','grape','grapefruit','starfruit','mandarin','blood_orange','lychee','blueberry','pineapple','apple','pumpkin','avocado','sweetberry','tamarind','papaya','redlove','mana_berry','kiwi','orange','yucca','aloe','coconut','persimmon','pomegranate','raspberry','pomelo','lemon','watermelon','vanilla','banana','cherry','blackberry','dragonfruit','gooseberry','guava','passionfruit','lavender','citron','barberry','oreo']
     // Ingredients.forEach(Ingredient => {
